fea1ebba40c36aaed4d6764b9df29bd32522a36c
apaulinhacarlos <apaulinhacarlos@gmail.com>
2024-06-19 12:23:56 -0300

    create http status map

--- backend/src/controllers/animals.controller.ts
+++ backend/src/controllers/animals.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import getHttpStatus from '../utils/http.map';
 import AnimalsService from '../services/animals.service';
 
 class AnimalsController {
@@ -8,32 +9,32 @@ class AnimalsController {
 
   public async get(req: Request, res: Response): Promise<Response> {
     const animals = await this.service.get();
-    return res.status(200).json(animals);
+    return res.status(getHttpStatus(animals.status)).json(animals.data);
   }
 
   public async getById(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
     const animal = await this.service.getById(Number(id));
-    return res.status(200).json(animal);
+    return res.status(getHttpStatus(animal.status)).json(animal.data);
   }
 
   public async create(req: Request, res: Response): Promise<Response> {
     const newAnimal = req.body;
     const animal = await this.service.create(newAnimal);
-    return res.status(201).json(animal);
+    return res.status(getHttpStatus(animal.status)).json(animal.data);
   }
 
   public async update(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
     const newAnimal = req.body;
     const animal = await this.service.update(Number(id), newAnimal);
-    return res.status(200).json(animal);
+    return res.status(getHttpStatus(animal.status)).json(animal.data);
   }
 
   public async delete(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
-    await this.service.delete(Number(id));
-    return res.status(204).send();
+    const result = await this.service.delete(Number(id));
+    return res.status(getHttpStatus(result.status)).send(result.data);
   }
 }
 
--- backend/src/services/animals.service.ts
+++ backend/src/services/animals.service.ts
@@ -7,26 +7,42 @@ class AnimalsService {
 
   public async get() {
     const animals = await this.model.get();
-    return animals;
+    return {
+      status: 'OK',
+      data: animals
+    };
   }
 
   public async getById(id: number) {
     const animal = await this.model.getById(id);
-    return animal;
+    return {
+      status: 'OK',
+      data: animal
+    };
   }
 
   public async create(newAnimal: any) {
     const animal = await this.model.create(newAnimal);
-    return animal;
+    return {
+      status: 'CREATED',
+      data: animal
+    };
   }
 
   public async update(id: number, newAnimal: any) {
     const animal = await this.model.update(id, newAnimal);
-    return animal;
+    return {
+      status: 'OK',
+      data: animal
+    };
   }
 
   public async delete(id: number) {
     await this.model.delete(id);
+    return {
+      status: 'NO_CONTENT',
+      data: null
+    };
   }
 }
 
