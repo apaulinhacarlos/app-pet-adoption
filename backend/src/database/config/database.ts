27d42dce09e8124fd9a0d68c8d5bef8de2474d7e
apaulinhacarlos <apaulinhacarlos@gmail.com>
2024-06-27 23:38:00 -0300

    fix: remove vercel e producao

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -1,31 +1,31 @@
 import { Options } from 'sequelize';
-import pg from 'pg';
-
-// const config: Options = {
-//   username: process.env.MYSQL_USER || 'root',
-//   password: process.env.MYSQL_PASSWORD || '123456',
-//   host: process.env.MYSQL_HOST || 'localhost',
-//   port: Number(process.env.MYSQL_PORT) || 3306,
-//   database: process.env.MYSQL_NAME || 'PET-ADOPTION',
-//   dialect: 'mysql',
-//   dialectOptions: {
-//     timezone: 'Z',
-//   },
-//   logging: false,
-// }
+// import pg from 'pg';
 
 const config: Options = {
-  username: process.env.POSTGRES_USER || 'root',
-  password: process.env.POSTGRES_PASSWORD || '123456',
-  host: process.env.POSTGRES_HOST || 'localhost',
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '123456',
+  host: process.env.MYSQL_HOST || 'localhost',
   port: Number(process.env.MYSQL_PORT) || 3306,
-  database: process.env.POSTGRES_DATABASE || 'PET-ADOPTION',
-  dialect: 'postgres',
-  dialectModule: pg,
+  database: process.env.MYSQL_NAME || 'PET-ADOPTION',
+  dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
   logging: false,
 }
+
+// const config: Options = {
+//   username: process.env.POSTGRES_USER || 'root',
+//   password: process.env.POSTGRES_PASSWORD || '123456',
+//   host: process.env.POSTGRES_HOST || 'localhost',
+//   port: Number(process.env.MYSQL_PORT) || 3306,
+//   database: process.env.POSTGRES_DATABASE || 'PET-ADOPTION',
+//   dialect: 'postgres',
+//   dialectModule: pg,
+//   dialectOptions: {
+//     timezone: 'Z',
+//   },
+//   logging: false,
+// }
 
 export = config;
